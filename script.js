3549630c3c9c916a5a8ff1b4094de67b512560b7
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-03-19 13:19:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-
+let yourArray  = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+	  {
+		  true:true,
+		  false:false
+	  }
+  ]
+];
