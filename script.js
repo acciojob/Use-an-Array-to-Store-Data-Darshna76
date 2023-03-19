45035bb696e71074ca5222242867930278dc169c
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-03-19 13:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-let yourArray  = [
-  [
-    {
-      one: 1,
-      two: 2
-    },
-    {
-      three: 3,
-      four: 4
-    }
-  ],
-  [
-    {
-      a: "a",
-      b: "b"
-    },
-    {
-      c: "c",
-      d: "d"
-    }
-	  {
-		  true:true,
-		  false:false
-	  }
-  ]
-];
+
+let simpleArray = ['one', 2, 'three', true, 3];
+console.log(simpleArray.length);
